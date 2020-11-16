c5a9c96030c9d9959ee0fa7af71cbe8d9eddd183
MexicanOwl64 <55261799+MexicanOwl64@users.noreply.github.com>
2020-11-16 12:44:14 -0600

    Fixed a few problems
    Fixed an issue I was having with exercise1.5

---
+++ Exercise1.5/Exercise1.5/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed;
+    private void Start()
+    {
+        speed = 10f;
+    }
+
+    private void Update()
+    {
+        
+       if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            
+                speed++;
+            
+                
+
+                Debug.Log("The Up arrow is being pressed");
+                Debug.Log(speed);
+            
+        }
+       else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            speed--;
+            Debug.Log("The down arrow is being pressed");
+            Debug.Log(speed);
+        }
+
+        float horizontal = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(horizontal, 0, 0);
+    }
+}
+
